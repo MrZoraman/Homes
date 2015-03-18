7c297acaa91022757455d77f447bbb5cedf8c1f4
Zora <MrZoraman@Gmail.com>
2015-03-17 22:30:01 -0600

    Database is set up.

--- src/main/java/com/lagopusempire/multihomes/MultiHomes.java
+++ src/main/java/com/lagopusempire/multihomes/MultiHomes.java
@@ -1,6 +1,8 @@
 package com.lagopusempire.multihomes;
 
+import com.lagopusempire.multihomes.config.ConfigKeys;
 import com.lagopusempire.multihomes.config.PluginConfig;
+import javax.persistence.PersistenceException;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
@@ -9,23 +11,52 @@ import org.bukkit.plugin.java.JavaPlugin;
  */
 public class MultiHomes extends JavaPlugin
 {
+
     @Override
     public void onEnable()
     {
         reload();
     }
-    
+
     @Override
     public void onDisable()
     {
-        
+
     }
-    
+
     public void reload()
     {
         getConfig().options().copyDefaults(true);
         saveConfig();
-        
+
         PluginConfig.setConfig(getConfig());
+        
+        if(PluginConfig.getBoolean(ConfigKeys.USE_DATABASE) || PluginConfig.getBoolean(ConfigKeys.USE_DATABASE))
+        {
+            setupDatabase();
+        }
+    }
+
+    private boolean setupDatabase()
+    {
+        try
+        {
+            getDatabase().find(com.lagopusempire.multihomes.homeIO.database.DBHome.class).findRowCount();
+        }
+        catch (PersistenceException ignored)
+        {
+            getLogger().info("Installing database for " + getDescription().getName() + " due to first time usage");
+            try
+            {
+                installDDL();
+            }
+            catch (RuntimeException e)
+            {
+                getLogger().severe(e.getMessage());
+                return false;
+            }
+        }
+        
+        return true;
     }
 }
