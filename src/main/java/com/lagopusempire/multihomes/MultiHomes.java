389aff0ebf8a64631dda298c411f07deb73a6f03
Zora <MrZoraman@Gmail.com>
2015-03-17 21:49:45 -0600

    Started the code for reloading. Default config options are saved.

--- src/main/java/com/lagopusempire/multihomes/MultiHomes.java
+++ src/main/java/com/lagopusempire/multihomes/MultiHomes.java
@@ -11,12 +11,18 @@ public class MultiHomes extends JavaPlugin
     @Override
     public void onEnable()
     {
-        
+        reload();
     }
     
     @Override
     public void onDisable()
     {
         
+    }
+    
+    public void reload()
+    {
+        getConfig().options().copyDefaults(true);
+        saveConfig();
     }
 }
