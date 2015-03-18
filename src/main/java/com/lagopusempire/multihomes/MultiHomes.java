c148df4fc0f6415fa82c2b745fb9c33057c3dee8
Zora <MrZoraman@Gmail.com>
2015-03-17 20:41:01 -0600

    Main class added

---
+++ src/main/java/com/lagopusempire/multihomes/MultiHomes.java
@@ -0,0 +1,22 @@
+package com.lagopusempire.multihomes;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author MrZoraman
+ */
+public class MultiHomes extends JavaPlugin
+{
+    @Override
+    public void onEnable()
+    {
+        
+    }
+    
+    @Override
+    public void onDisable()
+    {
+        
+    }
+}
