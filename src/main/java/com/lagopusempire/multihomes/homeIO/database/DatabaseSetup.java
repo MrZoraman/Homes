b0b71264d78f20cdb2e69b63e007bb21a2a72fc5
Zora <MrZoraman@Gmail.com>
2015-03-22 16:16:04 -0600

    Removed unused imports

--- src/main/java/com/lagopusempire/multihomes/homeIO/database/DatabaseSetup.java
+++ src/main/java/com/lagopusempire/multihomes/homeIO/database/DatabaseSetup.java
@@ -2,8 +2,6 @@ package com.lagopusempire.multihomes.homeIO.database;
 
 import com.lagopusempire.multihomes.config.ConfigKeys;
 import com.lagopusempire.multihomes.config.PluginConfig;
-import java.io.IOException;
-import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
