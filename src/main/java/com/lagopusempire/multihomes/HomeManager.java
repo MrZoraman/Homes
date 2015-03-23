441a3076e860bbc443992f86dd3cfe745d7879dc
Zora <MrZoraman@Gmail.com>
2015-03-22 18:49:19 -0600

    The rest of the methods are now implemented

--- src/main/java/com/lagopusempire/multihomes/HomeManager.java
+++ src/main/java/com/lagopusempire/multihomes/HomeManager.java
@@ -3,6 +3,8 @@ package com.lagopusempire.multihomes;
 import com.lagopusempire.multihomes.home.Home;
 import com.lagopusempire.multihomes.home.LoadResult;
 import com.lagopusempire.multihomes.homeIO.HomeIO;
+import com.lagopusempire.multihomes.homeIO.HomeListLoadedCallback;
+import com.lagopusempire.multihomes.homeIO.HomeLoadedCallback;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -14,7 +16,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 /**
@@ -23,76 +24,98 @@ import org.bukkit.event.player.PlayerQuitEvent;
  */
 public class HomeManager implements Listener
 {
+
     private static final int PLAYER_LOGIN_FAIL_TIMEOUT = 20 * 15;//15 seconds
-    
+
     private final Map<UUID, Map<String, Home>> homes = new HashMap<>();
-    
+
     private final HomeIO io;
     private final MultiHomes plugin;
-    
+
     public HomeManager(MultiHomes plugin, HomeIO io)
     {
         this.plugin = plugin;
         this.io = io;
     }
-    
+
     @EventHandler(priority = EventPriority.MONITOR)
     public void onPlayerJoinAsync(AsyncPlayerPreLoginEvent event)
     {
         boolean notifiedConsole = false;
         int failCounter = 0;
         //ASYNC
-        while(plugin.isEnabled() == false)
+        while (plugin.isEnabled() == false)
         {
-            if(!notifiedConsole)
+            if (!notifiedConsole)
             {
                 plugin.getLogger().warning("Prventing player " + event.getName() + " from joining the server because multihomes isn't loaded yet.");
                 notifiedConsole = true;
             }
-            
+
             failCounter++;
-            
-            if(failCounter >= PLAYER_LOGIN_FAIL_TIMEOUT)
+
+            if (failCounter >= PLAYER_LOGIN_FAIL_TIMEOUT)
             {
                 event.setKickMessage("Server is experiencing internal errors. Please try again later.");
                 event.setLoginResult(AsyncPlayerPreLoginEvent.Result.KICK_OTHER);
                 return;
             }
         }
-        
+
         final UUID uuid = event.getUniqueId();
-        
+
         //the plugin has been loaded at this point
-        io.loadHomes(event.getUniqueId(), (loadedHomes) -> 
+        io.loadHomes(event.getUniqueId(), (loadedHomes) ->
         {
             //SYNC
-            if(homes.get(uuid) == null)
+            if (homes.get(uuid) == null)
             {
                 homes.put(uuid, new HashMap<>());
             }
-            
+
             homes.get(uuid).putAll(loadedHomes);
         });
     }
-    
+
     @EventHandler
     public void onPlayerQuit(PlayerQuitEvent event)
     {
         homes.remove(event.getPlayer().getUniqueId());
     }
-    
-    public void setHome(Player player, UUID owner, String homeName)
+
+    public void setHome(Player player, UUID owner, String homeName, Runnable callback)
     {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        final Home home = new Home(owner, homeName, player.getLocation());
+        io.saveHome(home, callback);
+        homes.get(owner).put(homeName, home);
     }
-    
-    public Home getHome(UUID owner, String homeName)
+
+    public void getHome(UUID owner, String homeName, HomeLoadedCallback callback)
     {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (homes.containsKey(owner)) //True if the player is online
+        {
+            final Home home = homes.get(owner).get(homeName);
+            if (home != null)
+            {
+                callback.homeLoaded(home);
+            }
+            callback.homeLoaded(new Home(owner, homeName, LoadResult.DOES_NOT_EXIST));
+        }
+
+        //The player is offline
+        io.loadHome(owner, homeName, callback);
     }
-    
-    public List<String> getHomeList(UUID owner)
+
+    public void getHomeList(UUID owner, HomeListLoadedCallback callback)
     {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (homes.containsKey(owner))
+        {
+            final Set<String> homeSet = homes.get(owner).keySet();
+            final List<String> homeList = new ArrayList<>(homeSet);
+            java.util.Collections.sort(homeList);
+            callback.homeListLoaded(homeList);
+        }
+        
+        io.getHomeList(owner, callback);
     }
 }
