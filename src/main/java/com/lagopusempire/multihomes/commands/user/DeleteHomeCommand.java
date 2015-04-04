751495c27a92644b324bce0a29fcb743e5b2d79c
Zora <MrZoraman@Gmail.com>
2015-04-04 17:24:45 -0600

    delete home restructured

--- src/main/java/com/lagopusempire/multihomes/commands/user/DeleteHomeCommand.java
+++ src/main/java/com/lagopusempire/multihomes/commands/user/DeleteHomeCommand.java
@@ -3,18 +3,10 @@ package com.lagopusempire.multihomes.commands.user;
 import com.lagopusempire.multihomes.HomeManager;
 import com.lagopusempire.multihomes.MultiHomes;
 import com.lagopusempire.multihomes.commands.CommandBase;
-import com.lagopusempire.multihomes.commands.CommandFunctionBase;
 import com.lagopusempire.multihomes.config.ConfigKeys;
 import com.lagopusempire.multihomes.config.PluginConfig;
-import com.lagopusempire.multihomes.load.Loader;
-import com.lagopusempire.multihomes.messages.MessageFormatter;
-import com.lagopusempire.multihomes.messages.MessageKeys;
-import com.lagopusempire.multihomes.messages.Messages;
-import com.lagopusempire.multihomes.permissions.Permissions;
-import com.lagopusempire.multihomes.util.Util;
-import java.util.UUID;
+import com.lagopusempire.multihomes.jobs.user.DeleteHomeJob;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.java.JavaPlugin;
 
 /**
  *
@@ -36,93 +28,8 @@ public class DeleteHomeCommand extends CommandBase
                 ? args[0]
                 : PluginConfig.getString(ConfigKeys.IMPLICIT_HOME_NAME);
         
-        (new HomeDeleter(plugin, player, homeName, usingExplicitHome)).run();
-        
+        final DeleteHomeJob job = new DeleteHomeJob(plugin, homeManager, player, homeName, usingExplicitHome);
+        job.run();
         return true;
-    }
-    
-    private class HomeDeleter extends CommandFunctionBase
-    {
-        private final Loader loader;
-        private final Player player;
-        private final UUID uuid;
-        private final String homeName;
-        private final boolean usingExplicitHome;
-        
-        private volatile boolean somethingWasDeleted = false;
-        
-        public HomeDeleter(JavaPlugin plugin, Player player, String homeName, boolean usingExplicitHome)
-        {
-            super(plugin);
-            this.loader = new Loader(plugin);
-            this.player = player;
-            this.uuid = player.getUniqueId();
-            this.homeName = homeName;
-            this.usingExplicitHome = usingExplicitHome;
-            
-            final boolean isAsync = homeManager.shouldBeAsync();
-            
-            loader.addStep(this::checkPermissions, false);
-            loader.addStep(this::verifyLoaded, false);
-            loader.addStep(this::deleteHome, isAsync);
-            loader.addStep(this::notifyPlayer, false);
-        }
-        
-        void run()
-        {
-            loader.load(null);
-        }
-        
-        private boolean checkPermissions()
-        {
-            return Util.checkPerms(player, Permissions.DELETE_HOME);
-        }
-        
-        private boolean verifyLoaded()
-        {
-//            if(!homeManager.isLoaded(uuid))
-//            {
-//                final MessageFormatter formatter = Messages.getMessage(MessageKeys.NOT_LOADED_SELF)
-//                        .colorize();
-//                Util.sendMessage(player, formatter);
-//                return false;
-//            }
-//            
-            return true;
-        }
-        
-        private boolean deleteHome()
-        {
-            somethingWasDeleted = homeManager.deleteHome(uuid, homeName);
-            return true;
-        }
-        
-        private boolean notifyPlayer()
-        {
-            if(!somethingWasDeleted)//The home didn't exist
-            {
-                final MessageKeys key = usingExplicitHome
-                        ? MessageKeys.HOME_DELETE_NOEXIST_EXPLICIT
-                        : MessageKeys.HOME_DELETE_NOEXIST_IMPLICIT;
-                
-                final MessageFormatter formatter = Messages.getMessage(key)
-                        .colorize()
-                        .replace("home", homeName);
-                
-                player.sendMessage(formatter.toString());
-                return true;
-            }
-            
-            final MessageKeys key = usingExplicitHome
-                    ? MessageKeys.HOME_DELETE_SUCCESS_EXPLICIT
-                    : MessageKeys.HOME_DELETE_SUCCESS_IMPLICIT;
-            
-            final MessageFormatter formatter = Messages.getMessage(key)
-                    .colorize()
-                    .replace("home", homeName);
-            
-            player.sendMessage(formatter.toString());
-            return true;
-        }
     }
 }
---
+++ src/main/java/com/lagopusempire/multihomes/jobs/user/DeleteHomeJob.java
@@ -0,0 +1,71 @@
+package com.lagopusempire.multihomes.jobs.user;
+
+import com.lagopusempire.multihomes.HomeManager;
+import com.lagopusempire.multihomes.jobs.JobBase;
+import com.lagopusempire.multihomes.load.Loader;
+import com.lagopusempire.multihomes.messages.MessageFormatter;
+import com.lagopusempire.multihomes.messages.MessageKeys;
+import com.lagopusempire.multihomes.messages.Messages;
+import com.lagopusempire.multihomes.permissions.Permissions;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author MrZoraman
+ */
+public class DeleteHomeJob extends JobBase
+{
+    private final boolean usingExplicitHome;
+    
+    private volatile boolean somethingWasDeleted;
+
+    public DeleteHomeJob(JavaPlugin plugin, HomeManager homeManager, Player player, String homeName, boolean usingExplicitHome)
+    {
+        super(plugin, homeManager, player, homeName);
+        this.usingExplicitHome = usingExplicitHome;
+
+        this.setRequiredPermissions(Permissions.DELETE_HOME);
+    }
+
+    @Override
+    protected void addSteps(Loader loader)
+    {
+        loader.addStep(this::deleteHome, homeManager.shouldBeAsync());
+    }
+
+    private boolean deleteHome()
+    {
+        somethingWasDeleted = homeManager.deleteHome(uuid, homeName);
+        return true;
+    }
+
+    @Override
+    protected boolean notifyPlayer()
+    {
+        if (!somethingWasDeleted)//The home didn't exist
+        {
+            final MessageKeys key = usingExplicitHome
+                    ? MessageKeys.HOME_DELETE_NOEXIST_EXPLICIT
+                    : MessageKeys.HOME_DELETE_NOEXIST_IMPLICIT;
+
+            final MessageFormatter formatter = Messages.getMessage(key)
+                    .colorize()
+                    .replace("home", homeName);
+
+            player.sendMessage(formatter.toString());
+            return true;
+        }
+
+        final MessageKeys key = usingExplicitHome
+                ? MessageKeys.HOME_DELETE_SUCCESS_EXPLICIT
+                : MessageKeys.HOME_DELETE_SUCCESS_IMPLICIT;
+
+        final MessageFormatter formatter = Messages.getMessage(key)
+                .colorize()
+                .replace("home", homeName);
+
+        player.sendMessage(formatter.toString());
+        return true;
+    }
+}
